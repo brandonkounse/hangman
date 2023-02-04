20a50924670609e0da78fd2ccaf3bb1865ae55de
Brandon <kounse@gmail.com>
2023-02-04 17:40:36 -0600

    Created initialization of @dictionary and selected only the words between 5 and 12 characters long.

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
+# interface for player to interact with hangman game
 class Hangman
-  
+  attr_reader :dictionary
+
+  def initialize
+    @dictionary = File.readlines('google-10000-english-no-swears.txt', chomp: true)
+    @dictionary.select! { |words| words.length >= 5 && words.length <= 12 }
+  end
 end
