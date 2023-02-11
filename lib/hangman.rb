55b9357582c857c72f5a1f8785eba95605d7b7e9
Brandon <kounse@gmail.com>
2023-02-11 13:17:45 -0600

    Added MAX_TURNS constant. Created @turn instance variable. Renamed start method to setup. Created play method to get player input until max turns are reached. Created new_session to provide hidden word on a new game. Created private methods for obtaining player input and checking the validation of the input.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,10 +5,13 @@ require './lib/player'
 
 # interface for player to interact with hangman game
 class Hangman
-  attr_reader :words, :hidden_word, :player
+  MAX_TURNS = 7
+
+  attr_reader :words, :hidden_word, :player, :turn
 
   def initialize
-    word_bank
+    create_word_bank
+    @turn = 0
   end
 
   def add_player
@@ -17,32 +20,61 @@ class Hangman
     @player = Player.new
   end
 
-  def start
+  def setup
     print 'Would you like to start a new game[1] or load[2] a previous game? '
     case @player.input
     when '1'
-      @hidden_word = @words[rand(@words.length)]
+      new_session
     when '2'
-      load
+      load_session
     else
       puts 'Invalid Selection'
-      start
+      setup
     end
   end
 
-  def load
-    # load player file
-  end
-
-  def save
-    # save progress
+  def play
+    until @turn >= MAX_TURNS
+      obtain_player_input
+      p @turn += 1
+    end
   end
 
   private
 
   # read in source file and load words 5-12 letters in length
-  def word_bank
+  def create_word_bank
     @words = File.readlines('google-10000-english-no-swears.txt', chomp: true)
     @words.select! { |words| words.length >= 5 && words.length <= 12 }
+  end
+
+  def new_session
+    @hidden_word = @words[rand(@words.length)]
+  end
+
+  def load_session
+    # load player file
+  end
+
+  def save_session
+    # save progress
+  end
+
+  def obtain_player_input
+    guess = @player.input
+    if check_input(guess)
+      obtain_player_input
+    else
+      guess
+    end
+  end
+
+  def check_input(input)
+    if input.length > 1 || !input.match?(/[A-Za-z]/)
+      puts 'Input must match 1 single letter!'
+      true
+    elsif input.downcase.match?(/[a-z]/)
+      false
+    end
   end
 end
